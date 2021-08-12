e707274e6837314b1acbf53a4498e83a49a92565
Sergio Parra <sergiolparra666@gmail.com>
2021-08-11 22:01:33 -0430

    Add movimiento aleatorio

--- logic.js
+++ logic.js
@@ -5,7 +5,7 @@ function searchMove () {
 		x = Math.round(Math.random()*2)
 		y = Math.round(Math.random()*2)
 		let c = document.getElementById('c'+ x + y)
-		console.log(c)
+		// console.log(c)
 		if(c.getAttribute('title') == 0) cellAvilable = true
 	}
 	cell(x,y)
--- script_play.js
+++ script_play.js
@@ -1,55 +1,50 @@
 const $cell = document.querySelectorAll('.cell')
 const $clean = document.getElementById('clean')
 $clean.addEventListener('click',Dashboard)
-let $arr = [[],[]], $turn = 'ball' 
+let $arr = [], $turn 
 
 function Dashboard () {
-	$arr = [[],[]]
 	for (var i = 0; i < $cell.length; i++) {
 		$cell[i].setAttribute('title','0')
 		$cell[i].innerHTML = ''
 	}
 }
-	Dashboard()
 
 function cell (x,y) {
 	let c = document.getElementById('c' + x + y)
-	// let turn = turnPlay()
-	// console.log('valor c ')
-	// console.log(c)
 	if (c.getAttribute('title') == 0) {
-		// console.log($turn)
-			c.innerHTML = '<img src="img/' + $turn + '.svg" alt="">'
-		
+		console.log($turn)
+		c.innerHTML = '<img src="img/' + $turn + '.svg" alt="">'
 		if ($turn == 'ball'){
 			c.setAttribute('title','1')
 			$turn = 'cross'
-			searchMove()
-			$arr[0].push(c)
+			$arr.push(c)
+			console.log($arr.pop())
 		}else {
 			c.setAttribute('title','2')
-			$arr[1].push(c)
+			$arr.push(c)
 			$turn = 'ball'
-
+			console.log($arr.pop())
+			searchMove()
 		}
 	}else {
 		cellBusy($arr,$turn)
 	}
 }
 function cellBusy (array,player) {
-	// console.log('ocupado')
-	console.log(player)
-	if(player == 'cross'){
-		let q = array[1].length
-		// console.log(array[1][q-1],player)
-
-	}else {
-		let q = array[0].length
-		// console.log(array[0][q-1],player)
-
-	}
+	console.log(array.pop())
 }
-	
+function autoPlay () {
+	$turn = 'ball'
+	$arr = []
+
+	Dashboard()
+	searchMove()
+	// cell(1,2)
+
+}
+
+autoPlay()
 
 // State of Cell:
 /*
